11e107cb2bcd8ecc538a451fab47083704071d44
Daniyal Khan <dkay7223@gmail.com>
2023-01-24 22:40:30 +0500

    Update NotFound.jsx

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './notFound.css'
 const NotFound = () => {
   return (
     <h1>Page Not Found:</h1>
+    <Link to={path} className="btn sm"> Go to Homepage </Link>
+    
   )
 }
 
